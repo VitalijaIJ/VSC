66f498abb033f91b98bac2bcb110aee23ca0809d
VitalijaIJ <93917255+VitalijaIJ@users.noreply.github.com>
2021-11-14 11:35:23 +0200

    Update SeleniumCheckBoxPage.cs

--- AutomatinisTestavimas/Page/SeleniumCheckBoxPage.cs
+++ AutomatinisTestavimas/Page/SeleniumCheckBoxPage.cs
@@ -1,9 +1,11 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutomatinisTestavimas.Page
@@ -13,7 +15,7 @@ namespace AutomatinisTestavimas.Page
         private IWebDriver _driver;
         private IWebElement _checkBox => _driver.FindElement(By.Id("isAgeSelected"));
         private IWebElement _text => _driver.FindElement(By.Id("txtAge"));       
-        private IWebElement _firstCheckbox => _driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement _firstCheckbox => _driver.FindElement(By.Id(".cb1-element"));
         private IReadOnlyCollection<IWebElement> _multipleCheckboxList => _driver.FindElements(By.ClassName(".cb1-element"));
         private IWebElement _button => _driver.FindElement(By.Id("check1"));
         private IWebElement _resultFromPage => _driver.FindElement(By.Id("value"));
@@ -33,20 +35,23 @@ namespace AutomatinisTestavimas.Page
         public void FirstCheckBoxClick()
         {
             if (_firstCheckbox.Selected)
-                _firstCheckbox.Click();
+                _firstCheckbox.Click();            
         }
-        public void MultipleCheckboxClick()
+        public SeleniumCheckBoxPage MultipleCheckboxClick()
         {
+            FirstCheckBoxClick();
             foreach (IWebElement element in _multipleCheckboxList)
             {
+                if(!element.Selected)
                 element.Click();
             }
+            return this;
         }
-        public void ButtonName(string text)
+        public void ButtonName()
         {
-            Assert.IsTrue(_button.Text.Equals("Uncheck All"));
+            Assert.IsTrue(_button.GetAttribute("value").Equals("Uncheck All"), "Second is wrong");
         }
-            
+
         public void ButtonClick()
         {
             if (_button.GetAttribute("value") == "Uncheck All")
@@ -67,3 +72,4 @@ namespace AutomatinisTestavimas.Page
 
     }
 }
+
